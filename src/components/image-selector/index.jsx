bb6793d12f51d962125cc9ac23ff16b577664d17
sotopro <dasg.1993@gmail.com>
2022-10-17 18:48:47 -0500

    [feature] - image selector implement expo image picker

--- src/components/image-selector/index.jsx
+++ src/components/image-selector/index.jsx
@@ -1,11 +1,37 @@
+import * as ImagePicker from "expo-image-picker";
 import React, { useState } from "react";
-import { View, Text, Image, Button } from "react-native";
+import { View, Text, Image, Button, Alert } from "react-native";
 
 import colors from "../../utils/colors";
 import { styles } from "./styles";
 
 const ImageSelector = ({ onImage }) => {
   const [pickedUrl, setPickedUrl] = useState();
+
+  const verifyPermissions = async () => {
+    const { status } = await ImagePicker.requestCameraPermissionsAsync();
+
+    if (status !== "granted") {
+      Alert.alert("you need to grant camera permissions to use this app", [{ text: "Okay" }]);
+      return false;
+    }
+    return true;
+  };
+
+  const onHandleTakePhoto = async () => {
+    const hasPermission = await verifyPermissions();
+    if (!hasPermission) return;
+
+    const image = await ImagePicker.launchCameraAsync({
+      allowsEditing: true,
+      aspect: [16, 9],
+      quality: 0.5,
+    });
+
+    setPickedUrl(image.uri);
+    onImage(image.uri);
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.preview}>
@@ -15,7 +41,7 @@ const ImageSelector = ({ onImage }) => {
           <Image style={styles.image} source={{ uri: pickedUrl }} />
         )}
       </View>
-      <Button title="Take Photo" color={colors.secondary} onPress={() => {}} />
+      <Button title="Take Photo" color={colors.secondary} onPress={onHandleTakePhoto} />
     </View>
   );
 };
--- src/components/image-selector/styles.js
+++ src/components/image-selector/styles.js
@@ -1,7 +1,22 @@
 import { StyleSheet } from "react-native";
 
+import colors from "../../utils/colors";
+
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  preview: {
+    width: "100%",
+    height: 200,
+    marginBottom: 10,
+    justifyContent: "center",
+    alignItems: "center",
+    borderColor: colors.primary,
+    borderWidth: 1,
+  },
+  image: {
+    width: "100%",
+    height: "100%",
   },
 });
