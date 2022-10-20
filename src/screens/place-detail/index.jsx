d9726c22814782b9b717de1e931c5461fdb87f9c
sotopro <dasg.1993@gmail.com>
2022-10-19 19:21:33 -0500

    [feature] - create place detail screen & style

--- src/screens/place-detail/index.jsx
+++ src/screens/place-detail/index.jsx
@@ -1,12 +1,27 @@
-import { View, Text } from "react-native";
+import { View, Text, ScrollView, Image } from "react-native";
+import { useSelector } from "react-redux";
 
+import { MapPreview } from "../../components";
 import { styles } from "./styles";
 
-const PlaceDetail = ({ navigation }) => {
+const PlaceDetail = ({ navigation, route }) => {
+  const { placeId } = route.params;
+  const place = useSelector((state) =>
+    state.place.places.find((placeItem) => placeItem.id === placeId)
+  );
+
   return (
-    <View style={styles.container}>
-      <Text>Place Detail</Text>
-    </View>
+    <ScrollView style={styles.container}>
+      <Image source={{ uri: place.image }} style={styles.image} />
+      <View style={styles.content}>
+        <View style={styles.addressContainer}>
+          <Text style={styles.address}>{place.address}</Text>
+        </View>
+        <MapPreview style={styles.map} location={{ lat: place.coords.lat, lng: place.coords.lng }}>
+          <Text>Location it not available</Text>
+        </MapPreview>
+      </View>
+    </ScrollView>
   );
 };
 
--- src/screens/place-detail/styles.js
+++ src/screens/place-detail/styles.js
@@ -1,9 +1,34 @@
 import { StyleSheet } from "react-native";
 
+import colors from "../../utils/colors";
+
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+  },
+  image: {
+    height: "40%",
+    width: "100%",
+    minHeight: 300,
+  },
+  content: {
+    width: "100%",
+    backgroundColor: colors.white,
+    shadowOpacity: 0.26,
+    shadowColor: colors.black,
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    elevation: 5,
+    borderRadius: 10,
+  },
+  addressContainer: {
+    padding: 15,
+  },
+  address: {
+    color: colors.primary,
+    textAlign: "center",
+  },
+  map: {
+    height: 300,
   },
 });
