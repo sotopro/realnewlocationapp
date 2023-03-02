646896b7c7ca094c9f29b5574b0d719ab78b091f
Daniel Soto <dasg.1993@gmail.com>
2023-03-01 20:10:53 -0500

    [feature] - implement place details and styles component

--- src/screens/place-detail/index.jsx
+++ src/screens/place-detail/index.jsx
@@ -1,12 +1,25 @@
-import { View, Text } from "react-native";
+import { ScrollView, Text, View, Image } from "react-native";
+import { useSelector } from "react-redux";
 
+import { MapPreview } from "../../components/index";
 import { styles } from "./styles";
 
-const PlaceDetail = ({ navigation }) => {
+const PlaceDetail = ({ navigation, route }) => {
+  const { placeId } = route.params;
+  const place = useSelector((state) => state.place.places.find((place) => place.id === placeId));
+
   return (
-    <View style={styles.container}>
-      <Text>Place Detail</Text>
-    </View>
+    <ScrollView style={styles.container}>
+      <Image source={{ uri: place.image }} style={styles.image} />
+      <View style={styles.location}>
+        <View style={styles.addressContainer}>
+          <Text style={styles.address}>{place.address}</Text>
+        </View>
+        <MapPreview style={styles.map} location={{ lat: place.coords.lat, lng: place.coords.lng }}>
+          <Text>Ubicacion no disponible</Text>
+        </MapPreview>
+      </View>
+    </ScrollView>
   );
 };
 
